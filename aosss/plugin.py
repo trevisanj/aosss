fba76cff5fda2042befaeaefa71e6610346fd3e4
trevisanj <juliotrevisan@gmail.com>
2016-11-30 22:18:32 -0200

    astroapi and pyfant

--- aosss/plugin.py
+++ aosss/plugin.py
@@ -1,6 +1,6 @@
 from aosss.datatypes import *
 
-def setup_astroapi(aa):
+def _setup_astroapi(aa):
     """Adds entries to the astroapi module"""
 
     aa.classes_bin.extend([FileFullCube, FileSparseCube, FileSpectrumList])
