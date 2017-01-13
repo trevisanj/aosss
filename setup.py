3b8c279ea1abdbdeeb1c26221fde368f8cd3e36c
Julio Trevisan <juliotrevisan@gmail.com>
2017-01-13 11:50:37 -0200

    meeting BLB

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 from glob import glob
 
+NAME = 'aosss'
 setup(
-    name = 'aosss',
+    name =NAME,
     packages = find_packages(),
     version = '0.17.1.4',
     include_package_data=True,
@@ -14,5 +15,5 @@ setup(
     url = 'https://github.com/trevisanj/aosss', # use the URL to the github repo
     keywords= ['astronomy' 'adaptive optics'],
     install_requires = ['numpy', 'scipy', 'astropy', 'matplotlib', 'fortranformat'],  # matplotlib never gets installed correctly by pip, but anyway...
-    scripts = glob('scripts/*.py')  # Considers system scripts all .py files in 'scripts' directory
+    scripts = glob(NAME+'/scripts/*.py')  # Considers system scripts all .py files in 'scripts' directory
 )
